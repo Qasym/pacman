bba173f1ce90722e31418a9df7879d08069ea89a
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2022-02-21 20:46:54 +0900

    added some comments; collision is now fixed; todo added

--- src/com/Game/entity/Pacman.java
+++ src/com/Game/entity/Pacman.java
@@ -65,6 +65,20 @@ public class Pacman extends Entity {
         handler.getGameCamera().centerOnEntity(this);
 
         // We have to update collision box each tick to keep up with the sprite (which is also updated each tick)
+        /*
+        * Position of collision box and where it is being drawn is actually different values
+        * When we draw we assume the coordinate system of a screen, not the entire world coordinates
+        * For instance, let's say our world and our screen both originate from [0, 0]
+        * When we move pacman left enough, our camera will start to move leftwards
+        * What does it mean for the screen and for the world coordinates?
+        * The screen coordinate system is always the same, and it is used for rendering purposes
+        * But, since our screen !visually! moved to the left one of the coordinate systems must change
+        * because we see the different thing. We know that the screen stays constant all the time, so
+        * the world coordinate system has to change.
+        * Depending on how much we moved to the left, the origin of the world now is located at
+        * [xOffset, yOffset]
+        *
+        * We have to be careful what coordinate we use when dealing with rendering and ticking */
         collisionBox.x = (int)(x + DEFAULT_COLLISION_BOUNDS_X);
         collisionBox.y = (int)(y + DEFAULT_COLLISION_BOUNDS_Y);
     }
@@ -99,43 +113,66 @@ public class Pacman extends Entity {
                     collisionBox.width, collisionBox.height);
     }
 
+    /*! CHANGEABLE COMMENTS !
+    *
+    * Initially we check for the state of our key manager (up/down/left/right)
+    * Without loss of generality, we assume state is up
+    * We enter the corresponding if statement, and the fun begins
+    *
+    * We first check the position of our collision box, specifically its top left and right corners
+    * because those are the corners that are going to collide with the wall pacman approaching.
+    * In order to avoid getting inside the wall we have to check for the collision one step ahead,
+    * otherwise (if we check after making a step) we will make a step inside the wall and then understand
+    * that we have collided, which is not the desired behavior since it blocks our movement
+    * to the left and right (top left & right corners are inside the wall, so we can only move downwards)
+    * For that reason, we add the speed to the position of our points.
+    * When we move upwards, there is no change in x-axis, so we don't add speed to the x coordinate
+    * We only add speed to the y coordinate and check for the collision ahead, if we collide (or join the wall, so to say)
+    * in our next step we simply stop, that will allow our corners to be not inside the wall but rather
+    * right before it, so we can move to the left and to the right freely
+    * */
     public void move() {
+        // todo: make it so pacman doesn't stop movement if a player moves towards side walls
+        // for ex. if pacman is in the corridor moving upwards, current implementation will stop
+        // pacman when the player tries to move right, towards the wall
+        // we need to make it that pacman continues to move upwards
         if (handler.getKeyManager().up) {
             // if we move upwards, we have to check top left&right corners of collision box
-            if (!collidesWithTile((int)(speed + collisionBox.x) / Tile.TILE_WIDTH, // top left corner
-                                    (int)(speed + collisionBox.y) / Tile.TILE_HEIGHT)
+            if (!collidesWithTile(collisionBox.x / Tile.TILE_WIDTH, // top left corner
+                                    (int)(collisionBox.y - speed) / Tile.TILE_HEIGHT)
                 && // or
-                !collidesWithTile((int)(speed + collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // top right corner
-                                    (int)(speed + collisionBox.y) / Tile.TILE_HEIGHT)) {
+                !collidesWithTile((collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // top right corner
+                                    (int)(collisionBox.y - speed) / Tile.TILE_HEIGHT)) {
                 y -= speed;
                 pacmanSprite = Assets.getPacmanUp();
             }
         } else if (handler.getKeyManager().right) {
             // if we move rightwards, we have to check for top&bottom right corners
             if (!collidesWithTile((int)(speed + collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // top right corner
-                                    (int)(speed + collisionBox.y) / Tile.TILE_HEIGHT)
+                                    collisionBox.y / Tile.TILE_HEIGHT)
                 && //or
                 !collidesWithTile(  (int)(speed + collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // bottom right corner
-                                    (int)(speed + collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)) {
+                                    (collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)) {
                 x += speed;
                 pacmanSprite = Assets.getPacmanRight();
             }
         } else if (handler.getKeyManager().left) {
             // if we move leftwards, we have to check for top&bottom left corners
-            if (!collidesWithTile((int)(speed + collisionBox.x) / Tile.TILE_WIDTH, // top left corner
-                                    (int)(speed + collisionBox.y) / Tile.TILE_HEIGHT)
+            if (!collidesWithTile((int)(collisionBox.x - speed) / Tile.TILE_WIDTH, // top left corner
+                                    collisionBox.y / Tile.TILE_HEIGHT)
                 && //or
-                !collidesWithTile(  (int)(speed + collisionBox.x) / Tile.TILE_WIDTH, // bottom left corner
-                                    (int)(speed + collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)) {
+                !collidesWithTile(  (int)(collisionBox.x - speed) / Tile.TILE_WIDTH, // bottom left corner
+                                    (collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)) {
                 x -= speed;
                 pacmanSprite = Assets.getPacmanLeft();
             }
         } else if (handler.getKeyManager().down) {
             // if we move downwards, we have to check for bottom left&right corners
-            if (!collidesWithTile((int)(speed + collisionBox.x) / Tile.TILE_WIDTH, // bottom left corner
+            /*speed +*/
+            if (!collidesWithTile(collisionBox.x / Tile.TILE_WIDTH, // bottom left corner
                                     (int)(speed + collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)
                 && //or
-                !collidesWithTile(  (int)(speed + collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // bottom right corner
+                !collidesWithTile(  (collisionBox.x + collisionBox.width) / Tile.TILE_WIDTH, // bottom right corner
                                     (int)(speed + collisionBox.y + collisionBox.height) / Tile.TILE_HEIGHT)) {
                 y += speed;
                 pacmanSprite = Assets.getPacmanDown();
