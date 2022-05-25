2d55c877e8692fe709668b47b59401502faf3e77
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2022-05-25 13:58:07 +0900

    added MonsterBrain initialization; turning points

--- src/com/Game/states/GameState.java
+++ src/com/Game/states/GameState.java
@@ -1,14 +1,16 @@
 package com.Game.states;
 
-import com.Game.entity.moving.Pacman;
 import com.Game.gfx.Assets;
-import com.Game.tile.Tile;
+import com.Game.intelligence.MonsterBrain;
 import com.Game.ui.ImageButton;
 import com.Game.ui.UIManager;
 import com.Game.utils.Handler;
+import com.Game.utils.Utils;
 import com.Game.world.World;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
 
 public class GameState extends State {
     private World world;
@@ -16,7 +18,11 @@ public class GameState extends State {
 
     public GameState(Handler handler) {
         super(handler);
-        world = new World(handler,"res/worlds/base_world"); // initializing the world
+        // initializing the monster AI
+        initializeAI();
+
+        // initializing the world
+        world = new World(handler,"res/worlds/base_world");
         handler.setWorld(world);
 
         // initializing UI for when the game finishes
@@ -45,6 +51,21 @@ public class GameState extends State {
         if (handler.getWorld().getEntityManager().getPacman().isDead()) {
             uiManager.render(g);
         }
+    }
+
+    /*
+    * Every value at even index i is the x coordinate and
+    * i+1 is the corresponding y coordinate of a turning point
+    *
+    * Turning point is a tile at which monster can turn
+    * */
+    private void initializeAI() {
+        String[] file = Utils.loadFileAsString("res/worlds/base_world_turns.txt").split("\\s+");
+        Point[] points = new Point[file.length / 2];
+        for (int i = 0, j = 0; i < file.length; i += 2) {
+            points[j++] = new Point(Utils.parseInt(file[i]), Utils.parseInt(file[i + 1]));
+        }
+        MonsterBrain.setTurningPoints(points);
     }
 
     public UIManager getUiManager() {
